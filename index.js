06a01852eb98346ddadce69520bb5e5727dbc1a2
sinthyasithy <145050483+sinthyasithy@users.noreply.github.com>
2024-04-29 21:36:22 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const books = [];
+// object literal
+const car = {
+  "door" : 4,
+  "window" : 6,
+  "wheel" : 4,
+  "hasFuel" : true,
+  "seat" : ['front-seat','back-seat'],
+  "color" : 'red'
+}
+
+console.log(car.hasFuel)
